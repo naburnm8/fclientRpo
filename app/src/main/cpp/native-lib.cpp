db5ee2922e0fb30ce90cca1ef64174213363b098
Me <alint@bk.ru>
2025-02-21 12:03:03 +0300

    part1: working mbedtls

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,12 +3,19 @@
 #include <android/log.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/android_sink.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/des.h>
 
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "fclient_ndk", __VA_ARGS__)
 
 #define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
 
 auto android_logger = spdlog::android_logger_mt("android", "fclient_ndk");
+
+mbedtls_entropy_context entropy;
+mbedtls_ctr_drbg_context ctr_drbg;
+char *personalization = "fclient-sample-app";
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_ru_bmstu_naburnm8_fclient_MainActivity_stringFromJNI(
@@ -18,4 +25,24 @@ Java_ru_bmstu_naburnm8_fclient_MainActivity_stringFromJNI(
     LOG_INFO("Hello from c++ %d", 2025);
     SLOG_INFO("Hello from spdlog {0}", 2025);
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_ru_bmstu_naburnm8_fclient_MainActivity_initRng(JNIEnv *env, jclass clazz) {
+    mbedtls_entropy_init( &entropy );
+    mbedtls_ctr_drbg_init( &ctr_drbg );
+
+    return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
+                (const unsigned char *) personalization,
+                strlen( personalization ) );
+}
+
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_ru_bmstu_naburnm8_fclient_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
+    uint8_t * buf = new uint8_t [no];
+    mbedtls_ctr_drbg_random(&ctr_drbg, buf, no);
+    jbyteArray rnd = env->NewByteArray(no);
+    env->SetByteArrayRegion(rnd, 0, no, (jbyte *)buf);
+    delete[] buf;
+    return rnd;
 }
--- app/src/main/java/ru/bmstu/naburnm8/fclient/MainActivity.java
+++ app/src/main/java/ru/bmstu/naburnm8/fclient/MainActivity.java
@@ -1,15 +1,20 @@
 package ru.bmstu.naburnm8.fclient;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import ru.bmstu.naburnm8.fclient.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+        Log.println(Log.INFO, "Library Loaded", "Library Loaded");
     }
 
     private ActivityMainBinding binding;
@@ -21,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] v = randomBytes(10);
+
+        Log.println(Log.INFO, "mbedtls", Arrays.toString(v));
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
@@ -31,4 +41,6 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
 }
